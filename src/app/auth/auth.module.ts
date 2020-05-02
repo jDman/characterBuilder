96d645710fd89c90129cf5a3bb1e9c82ee337bbe
jDman <davis.jaym@gmail.com>
2020-05-02 11:37:43 +0100

    feat - add login and signup form component shells

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { SignupFormComponent } from './signup/signup-form/signup-form.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
 
 @NgModule({
-  declarations: [SignupComponent, LoginComponent],
+  declarations: [SignupComponent, LoginComponent, SignupFormComponent, LoginFormComponent],
   imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
