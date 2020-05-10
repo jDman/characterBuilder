eb4a22fcd20831374be991c43aed804e6c5a7efd
jDman <davis.jaym@gmail.com>
2020-05-10 19:23:31 +0100

    tests - amended character abilities test

--- src/app/builder/character-abilities/character-abilities.component.spec.ts
+++ src/app/builder/character-abilities/character-abilities.component.spec.ts
@@ -6,6 +6,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 describe('CharacterAbilitiesComponent', () => {
   let component: CharacterAbilitiesComponent;
   let fixture: ComponentFixture<CharacterAbilitiesComponent>;
+  let compiled: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -25,11 +26,22 @@ describe('CharacterAbilitiesComponent', () => {
     component.charisma = 4;
     component.isEditing = false;
 
+    compiled = fixture.nativeElement;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create and display abilities as list items', () => {
+    const listedAbilites = compiled.querySelectorAll(
+      '.character-abilities__list-item'
+    );
+
+    expect(listedAbilites[0].textContent.trim()).toBe('Strength: (6)');
+    expect(listedAbilites[1].textContent.trim()).toBe('Dexterity: (4)');
+    expect(listedAbilites[2].textContent.trim()).toBe('Constitution: (9)');
+    expect(listedAbilites[3].textContent.trim()).toBe('Intelligence: (7)');
+    expect(listedAbilites[4].textContent.trim()).toBe('Wisdom: (8)');
+    expect(listedAbilites[5].textContent.trim()).toBe('Charisma: (4)');
   });
 
   describe('editAbilities', () => {
